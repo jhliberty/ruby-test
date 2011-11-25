fdadf28f69f84e53ee21c05f6bf4601e283e362c
John-Henry Liberty <johnhenry.liberty@gmail.com>
2011-11-25 17:31:58 -0600

    update-city pulls in csv file and reads from it

--- update-city.rb
+++ update-city.rb
@@ -3,7 +3,7 @@ require "highline/import"
 require "yaml"
 require "active_record"
 require "mysql2"
-
+require "csv"
 
 @environment = ENV['RACK_ENV'] || 'development'
 @dbconfig = YAML.load(File.read('config/database.yml'))
@@ -18,6 +18,9 @@ end
 
 begin 
   
+  CSV.foreach("updates.csv", {:headers => true, :return_headers => false, :header_converters => :symbol, :converters => :all}) do |row|
+    puts "#{row[:state]}, #{row[:region]}, #{row[:updated_region]}"
+  end
   entry = Hash.new 
   entry[:state] = ask("State?  ") { |q| q.default = "none" }
   entry[:region]   = ask("City?  ") { |q| q.default = "none" }
@@ -31,16 +34,18 @@ begin
     puts message
     updates << message
   else
-    puts "#{@loc.count} records found with #{entry[:state]} and #{entry[:region]}"
+    puts "#{@loc.count} locations found with #{entry[:state]} and #{entry[:region]}"
     choices = %w{yes no}
     say("Would you like to rename city? [yes] [no]")
 
     case ask("?  ", choices)
     when "yes"
       entry[:updated_region] = ask("Updated City Name?  ") { |q| q.default = "" }
-      @loc.update_all(:region => entry[:updated_region])
-      @loc = Location.where(:state => entry[:state], :region => entry[:updated_region])
-      message = "#{@loc.count} records with state: #{entry[:state]} have had their region changed to #{entry[:updated_region]}"
+      @loc = Location.where(:state => entry[:state], :region => entry[:region]).first
+      @region = Region.where(:state_id => @loc.state_id, :region => @loc.region)
+      @region.update_all(:region => entry[:updated_region])
+      @region = Region.where(:state_id => @loc.state_id, :region =>entry[:updated_region])
+      message = "#{@region.count} region with state: #{entry[:state]} and region: #{entry[:region]} have had their region changed to #{entry[:updated_region]}"
       puts message
       updates << message
     else
