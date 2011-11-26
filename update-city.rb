aaabc1f139bc9a3d1e6f12796efffb6ab59f8bec
John-Henry Liberty <johnhenry.liberty@gmail.com>
2011-11-25 18:18:46 -0600

    import csv function works, updates records based on csv file

--- update-city.rb
+++ update-city.rb
@@ -17,44 +17,61 @@ end
     updates = [ ]
 
 begin 
+  entry = Hash.new
+  entry[:csv] = ask("Which CSV file would you like to use?  ")
   
-  CSV.foreach("updates.csv", {:headers => true, :return_headers => false, :header_converters => :symbol, :converters => :all}) do |row|
-    puts "#{row[:state]}, #{row[:region]}, #{row[:updated_region]}"
-  end
-  entry = Hash.new 
-  entry[:state] = ask("State?  ") { |q| q.default = "none" }
-  entry[:region]   = ask("City?  ") { |q| q.default = "none" }
-  
-  
-  puts entry
-  
-  @loc = Location.where(:state => entry[:state], :region => entry[:region])
-  if @loc.empty?
-    message = "No location found with #{entry[:state]} and #{entry[:region]}"
-    puts message
-    updates << message
-  else
-    puts "#{@loc.count} locations found with #{entry[:state]} and #{entry[:region]}"
-    choices = %w{yes no}
-    say("Would you like to rename city? [yes] [no]")
-
-    case ask("?  ", choices)
-    when "yes"
-      entry[:updated_region] = ask("Updated City Name?  ") { |q| q.default = "" }
-      @loc = Location.where(:state => entry[:state], :region => entry[:region]).first
-      @region = Region.where(:state_id => @loc.state_id, :region => @loc.region)
-      @region.update_all(:region => entry[:updated_region])
-      @region = Region.where(:state_id => @loc.state_id, :region =>entry[:updated_region])
-      message = "#{@region.count} region with state: #{entry[:state]} and region: #{entry[:region]} have had their region changed to #{entry[:updated_region]}"
-      puts message
-      updates << message
+  CSV.foreach("#{entry[:csv]}.csv", {:headers => true, :return_headers => false, :header_converters => :symbol, :converters => :all}) do |row|
+    @loc = Location.where(:state => row[:state], :region => row[:region]).first
+    if @loc.nil?
+      puts "No record found with #{row[:state]} and #{row[:region]}"
     else
-      message = "#{@loc.count} records found, but not updated with #{entry[:state]} and #{entry[:region]}"
-      puts message
-      updates << message
-    end    
+      @region = Region.where(:state_id => @loc.state_id, :region => @loc.region)
+      if @region.nil?
+        message = "No region matches #{@loc.region} and #{@loc.state}"
+      else
+        @region.update_all(:region => row[:updated_region])
+        @region = Region.where(:region => row[:updated_region], :state_id => @loc.state_id)
+        message = "#{@region.count} regions updated with #{@loc.state} and #{row[:updated_region]}"
+        puts message
+        updates << message
+      end
+    end
   end
-end while agree("Enter another update?  ", true)
+end
+#   entry = Hash.new 
+#   entry[:state] = ask("State?  ") { |q| q.default = "none" }
+#   entry[:region]   = ask("City?  ") { |q| q.default = "none" }
+#   
+#   
+#   puts entry
+#   
+#   @loc = Location.where(:state => entry[:state], :region => entry[:region])
+#   if @loc.empty?
+#     message = "No location found with #{entry[:state]} and #{entry[:region]}"
+#     puts message
+#     updates << message
+#   else
+#     puts "#{@loc.count} locations found with #{entry[:state]} and #{entry[:region]}"
+#     choices = %w{yes no}
+#     say("Would you like to rename city? [yes] [no]")
+# 
+#     case ask("?  ", choices)
+#     when "yes"
+#       entry[:updated_region] = ask("Updated City Name?  ") { |q| q.default = "" }
+#       @loc = Location.where(:state => entry[:state], :region => entry[:region]).first
+#       @region = Region.where(:state_id => @loc.state_id, :region => @loc.region)
+#       @region.update_all(:region => entry[:updated_region])
+#       @region = Region.where(:state_id => @loc.state_id, :region =>entry[:updated_region])
+#       message = "#{@region.count} region with state: #{entry[:state]} and region: #{entry[:region]} have had their region changed to #{entry[:updated_region]}"
+#       puts message
+#       updates << message
+#     else
+#       message = "#{@loc.count} records found, but not updated with #{entry[:state]} and #{entry[:region]}"
+#       puts message
+#       updates << message
+#     end    
+#   end
+# end while agree("Enter another update?  ", true)
 
   if agree("Save these updates?  ", true)
     file_name = ask("Enter a file name:  ") do |q|
